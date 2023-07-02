640a0eff158e35d50a08da7ee8c799b42087b08d
haruki3242 <89905195+haruki3242@users.noreply.github.com>
2023-07-02 19:41:56 +0900

    Add files via upload

---
+++ reserve.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>LINK 文化祭Tシャツ　２次受け付け</title>
+  <link rel="icon" href="icon2023.ico">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="./new.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+  <style>
+    .slide img {
+      width: 100%;
+    }
+  </style>
+ <meta http-equiv="refresh" content="0;URL=https://ngfes.com/reserve-finish">
+</head>
+
+<body>
+  <div class="form-wrapper">
+    <h1>LINK 文化祭Tシャツ<p><br>２次受け付け</h1>
+
+    <!-- 予約フォームのコード -->
+    <form action="confirm.php" method="POST">
+      <div class="form-item">
+        <label for="name">名前</label>
+        <input type="text" name="name" required="required" value="<?php echo htmlspecialchars($name); ?>" placeholder="奈良学太郎">
+      </div>
+      <div class="form-item">
+        <label for="name-kana">名前（ひらがな）</label>
+        <input type="text" name="name-kana" pattern="(?=.*?[\u3041-\u309F])[\u3041-\u309F]*" required="required" placeholder="ならがくたろう">
+      </div>
+      <div class="form-item">
+        <label for="student-number">学籍番号</label>
+        <input type="number" name="student-number" min="111111" max="999999" oninput="javascript: this.value = this.value.slice(0,6);" required="required" placeholder="数字6桁　靴箱の番号と同じです">
+      </div>
+
+      <div class="form-item">
+        <label for="size">サイズ</label><br>
+        <select name="size-select" required>
+          <option value="" disabled selected>サイズを選択してください</option>
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+          <option>XL</option>
+          <option>XXL</option>
+        </select>
+      </div>
+      <div class="form-item">
+        <label for"count">枚数</label><br>
+      <select id="count-select" name="count-select" onchange="updateColorOptions()" required>
+        <option value="" disabled selected>枚数を選択してください</option>
+        <option value="1" selected>1枚</option>
+        <option value="2">2枚</option>
+        <option value="3">3枚</option>
+        <option value="4">4枚</option>
+        <option value="5">5枚</option>
+      </select>
+    </div>
+    <div class="form-item">
+<div id="color-options">
+        <!-- カラーオプションはJavaScriptで動的に生成されます -->
+      </div>
+    </div>
+
+<div class="form-item">
+<label>カラーバリエーション（画像はイメージです）</label> </br>
+  <div class="slide-items__wrap">
+    <ul class="slide-items">
+      <li class="slide-items__item">
+        <img src="image1_1.jpg" alt="" title="ホワイト">
+      </li>
+      <li class="slide-items__item">
+        <img src="image1_2.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image2_1.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image2_2.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image3_1.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image3_2.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image4_1.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image4_2.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image5_1.jpg" alt="">
+      </li>
+      <li class="slide-items__item">
+        <img src="image5_2.jpg" alt="">
+      </li>
+    </ul>
+  </div>
+  <script>
+$(document).ready(function () {
+  $('.slide-items').bxSlider();
+  captions: true;
+});
+  </script>
+</div>
+
+    <?php if (isset($error)) : ?>
+      <div class="error-message"><?php echo $error; ?></div>
+    <?php endif; ?>
+    <div class="button-panel">
+      <input type="submit" class="button" title="Step2" value="STEP2（入力情報の確認）に進む"></input>
+    </div>
+  </form>
+  <div class="form-footer">
+    <p>&copy; 2023 奈良学園中学校・高等学校 文化祭実行委員会</p>
+  </div>
+    
+    <!-- 予約フォームのコード -->
+    <form action="confirm.php" method="POST">
+      <!-- 予約フォームの内容 -->
+    </form>
+  </div>
+  </div>
+</div>
+<script>
+  // フォームが読み込まれたときに初期のカラーオプションを生成する
+  window.onload = function() {
+    updateColorOptions();
+  };
+
+  function updateColorOptions() {
+    var countSelect = document.getElementById('count-select');
+    var selectedCount = countSelect.value;
+    var colorOptionsDiv = document.getElementById('color-options');
+    colorOptionsDiv.innerHTML = '';
+
+    if (selectedCount !== '') {
+      for (var i = 1; i <= selectedCount; i++) {
+        var label = document.createElement('label');
+        if (selectedCount === '1') {
+          label.textContent = 'カラー';
+        } else {
+          label.textContent = i + '枚目のカラー';
+        }
+        colorOptionsDiv.appendChild(label);
+
+        colorOptionsDiv.appendChild(document.createElement('br'));
+
+        var select = document.createElement('select');
+        select.name = 'color' + i;
+        select.required = true;
+        select.onchange = function() {
+          updateSampleImage(this);
+        };
+
+        var option1 = document.createElement('option');
+        option1.value = '';
+        option1.disabled = true;
+        option1.selected = true;
+        option1.textContent = 'カラーを選択してください';
+        select.appendChild(option1);
+
+        var option2 = document.createElement('option');
+        option2.value = 'a';
+        option2.textContent = 'ホワイト';
+        select.appendChild(option2);
+
+        var option3 = document.createElement('option');
+        option3.value = 'b';
+        option3.textContent = 'ベビーピンク';
+        select.appendChild(option3);
+
+        var option4 = document.createElement('option');
+        option4.value = 'c';
+        option4.textContent = 'ライトブルー';
+        select.appendChild(option4);
+
+        var option5 = document.createElement('option');
+        option5.value = 'd';
+        option5.textContent = 'ロイヤルブルー';
+        select.appendChild(option5);
+
+        var option6 = document.createElement('option');
+        option6.value = 'e';
+        option6.textContent = 'ブラック';
+        select.appendChild(option6);
+
+        colorOptionsDiv.appendChild(select);
+
+        colorOptionsDiv.appendChild(document.createElement('br'));
+      }
+    }
+  }
+
+  function updateSampleImage(select) {
+    var index = select.name.slice(-1);
+    var image = document.getElementById('sample-image' + index);
+
+    if (select.value === 'a') {
+      image.src = 'image1_1.jpg';
+    } else if (select.value === 'b') {
+      image.src = 'image2_1.jpg';
+    } else if (select.value === 'c') {
+      image.src = 'image3_1.jpg';
+    } else if (select.value === 'd') {
+      image.src = 'image4_1.jpg';
+    } else if (select.value === 'e') {
+      image.src = 'image5_1.jpg';
+    } else {
+      image.src = '';
+    }
+  }
+</script>
+
+</body>
+</html>
